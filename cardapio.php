4974de3f1fb7d14218ad5f6f1dc0706d0a59a14c
Gabriel Vinícius <gabriel_v_cruz@estudante.sesisenai.org.br>
2024-10-24 20:44:40 -0300

    listar cardapio

--- cardapio.php
+++ cardapio.php
@@ -5,23 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cardápio</title>
+
+    <link rel="stylesheet" href="dist/css/global.css">
+
 </head>
 <body>
     <?php
         include("php/funcoes.php");
     ?>
+    <a href="telainicial.php">Voltar</a>
     <h1>Cardápio</h1>
     <table>
         <thead>
         <tr>
-            <th>ID do Item</th>
-            <th>Valor Unitário</th>
             <th>Descrição</th>
-            <th>Disponibilidade</th>
-            <th>ID do Tipo de Item</th>
+            <th>Valor Unitário</th>
+            <th colspan="2">Opões</th>
         </tr>
         </thead>
         <tbody>
+            <?php echo carregaCardapio(); ?>
         </tbody>    
     </table>
 
