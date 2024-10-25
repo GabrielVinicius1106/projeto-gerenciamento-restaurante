3bc451edf92e6f2ecfcaf05a5ed465a2817ab55b
Gabriel Vinícius <gabriel_v_cruz@estudante.sesisenai.org.br>
2024-10-24 21:01:19 -0300

    adicionar item

---
+++ adicionarItem.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cárdapio | Adicionar Item</title>
+
+    <link rel="stylesheet" href="dist/css/global.css">
+
+</head>
+<body>
+    <a href="cardapio.php">Voltar</a>
+    <h1>Adicionar Item</h1>
+    <p>Nome: <input type="text" name="nItem" id=""></p>
+    <p>Preço: <input type="number" name="nValor" id=""></p>
+    <p>Categoria: <select name="nCategoria" id="">
+                    <option value=""></option>
+                    <option value="">Comida</option>
+                    <option value="">Bebida</option>
+                  </select>
+    </p>
+    <p>Disponibilidade: <select name="" id="">
+                            <option value=""></option>
+                            <option value="">Disponível</option>
+                            <option value="">Indisponível</option>
+                        </select>
+    </p>
+</body>
+</html>
