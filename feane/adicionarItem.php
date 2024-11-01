6010354b0dcd548d81d4a44424cd27c805a68b9a
DanDan <daniel_mn_costa@estudante.sc.senai.br>
2024-10-31 21:22:59 -0300

    hahahah

--- feane/adicionarItem.php
+++ feane/adicionarItem.php
@@ -12,26 +12,28 @@ include('php/funcoes.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cárdapio | Adicionar Item</title>
 
-    <link rel="stylesheet" href="dist/css/global.css">
+    <link rel="stylesheet" href="dist/css/elisson.css">
 
 </head>
 <body>
     <a href="cardapio.php">Voltar</a>
     <h1>Adicionar Item</h1>
-    <form action="php/crudItem.php?id=insert" method="POST">
-        <p>Descrição: <input type="text" name="nItem"></p>
-        <p>Preço: <input type="text" name="nValor"></p>
-        <p>Tipo de Item: 
-            <?php echo carregaCategorias();?>
-        </p>
-        <p>Disponibilidade: <select name="nDisponibilidade">
-                                <option value=""></option>
-                                <option value="1">Disponível</option>
-                                <option value="0">Indisponível</option>
-                            </select>
-        </p>
-        <input type="submit" value="Adicionar">
-        <input type="reset" value="Limpar">
-    </form>
+    <div class="container">
+        <form action="php/crudItem.php?id=insert" method="POST">
+            <p>Descrição: <input type="text" name="nItem"></p>
+            <p>Preço: <input type="text" name="nValor"></p>
+            <p>Tipo de Item: 
+                <?php echo carregaCategorias();?>
+            </p>
+            <p>Disponibilidade: <select name="nDisponibilidade">
+                                    <option value=""></option>
+                                    <option value="1">Disponível</option>
+                                    <option value="0">Indisponível</option>
+                                </select>
+            </p>
+            <input type="submit" value="Adicionar">
+            <input type="reset" value="Limpar">
+        </form>
+    </div>
 </body>
 </html>
