377dc9c2bab46d4cf4562cccf3c00f6390dfd31b
Gabriel Vinícius <gabriel_v_cruz@estudante.sesisenai.org.br>
2024-11-04 18:59:52 -0300

    modifica descricao - nome

--- feane/adicionarItem.php
+++ feane/adicionarItem.php
@@ -12,14 +12,15 @@ include('php/funcoes.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cárdapio | Adicionar Item</title>
 
-    <link rel="stylesheet" href="dist/css/global.css">
+    <link rel="stylesheet" href="dist/css/elisson.css">
 
 </head>
 <body>
     <a href="cardapio.php">Voltar</a>
     <h1>Adicionar Item</h1>
+<<<<<<< HEAD
     <form action="php/crudItem.php?id=insert" method="POST">
-        <p>Descrição: <input type="text" name="nItem"></p>
+        <p>Nome: <input type="text" name="nItem"></p>
         <p>Preço: <input type="text" name="nValor"></p>
         <p>Tipo de Item: 
             <?php echo carregaTiposItem();?>
@@ -34,5 +35,24 @@ include('php/funcoes.php');
         <input type="submit" value="Adicionar">
         <input type="reset" value="Limpar">
     </form>
+=======
+    <div class="container" id="additem">
+        <form action="php/crudItem.php?id=insert" method="POST">
+            <p>Descrição: <input type="text" name="nItem"></p>
+            <p>Preço: <input type="text" name="nValor"></p>
+            <p>Tipo de Item: 
+                <?php echo carregaCategorias();?>
+            </p>
+            <p>Disponibilidade: <select name="nDisponibilidade">
+                                    <option value=""></option>
+                                    <option value="1">Disponível</option>
+                                    <option value="0">Indisponível</option>
+                                </select>
+            </p>
+            <input type="submit" value="Adicionar">
+            <input type="reset" value="Limpar">
+        </form>
+    </div>
+>>>>>>> 836657f4008879aa132c5cc11ccc597f66724e5b
 </body>
 </html>
