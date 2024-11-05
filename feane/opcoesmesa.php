84466684b80f539e70f1e02e353b93f70bf35faf
DanDan <daniel_mn_costa@estudante.sc.senai.br>
2024-11-04 21:00:14 -0300

    nvvalt

--- feane/opcoesmesa.php
+++ feane/opcoesmesa.php
@@ -21,13 +21,7 @@
         <div class="container" id="additem">
             <p class="p1">Capacidade : <input type="text" value = "<?php echo carregaCapacidade($_GET['id']) ?>"></p>
             <p class="p2">Ocupação : <select name="nOcup">
-=======
-<a href="mesas.php">Voltar</a>
-<h1>Mesa <?php echo $id1 ?></h1>
-<div class="container" id="additem">
-<p class="p1">Capacidade : <input type="text" value = "<?php echo carregaCapacidade($_GET['id']) ?>"></p>
-<p class="p2">Ocupação : <select name="nOcup">
->>>>>>> 484221d1f3646773fc9534220f552ee52a5cedb6
+
                                 <option value="op1">1</option>
                                 <option value="op2">2</option>
                                 <option value="op3">3</option>
