56a9a56b61e9358a63763dd7e0ff51e89a0e7a81
luluMachuca <furlanluisgustavo03@gmail.com>
2024-11-07 20:49:30 -0300

    2

--- feane/opcoesmesa.php
+++ feane/opcoesmesa.php
@@ -16,15 +16,13 @@
 </head>
 <body>
 
-<form action="php/salvarocupacao.php?id=<?php echo $_GET["id"];?>" method="POST"></form>
-<a href="mesas.php">Voltar</a>
+<form action="php/salvarocupacao.php?id=<?php echo $_GET["id"];?>" method="POST">
+    <a href="mesas.php">Voltar</a>
     <h1>Mesa <?php echo $id1 ?></h1>
         <div class="container" id="additem">
             <p class="p2">Quantidade de pessoas :  <input type="number" name="nInput">
             <p><input type="submit" value="Salvar" ></p>                
-            </p>
         </div>
-
 </form>
 
 </body>
