568d40a32b684a074e1dbbbf96e7efb60f4f3cc6
Gabriel Vinícius <gabriel_v_cruz@estudante.sesisenai.org.br>
2024-11-18 20:54:22 -0300

    inserção de itens aos pedidos

--- feane/php/crudPedidoItem.php
+++ feane/php/crudPedidoItem.php
@@ -1,6 +1,5 @@
 <?php 
 
-include('conection.php');
 include('funcoes.php');
 
 $operacao = $_GET['operacao'];
@@ -12,8 +11,38 @@ $obs = $_POST['nObs'];
 // var_dump("Id Pedido: ".$idPedido,"Id Item: ".$idItem, $quantidadeItem, $obs);
 // die();
 
+$list = '';
+
 switch($operacao){
 
+    case 'insert': 
+        // Insert
+        $sql = "INSERT INTO pedido_item (quantidade_itens, obs_item, pedido_id_pedido, item_id_item)
+                    VALUES (".$quantidadeItem.", '".$obs."', ".$idPedido.", ".$idItem.");";
+        break;
+    case 'update':
+        // Update
+
+        break;
+    case 'delete':
+        // Delete
+
+        break;
+    default:
+        header('location: ../pedidos.php');
+        break;
 }
+
+                // var_dump($sql);
+                // die();
+
+
+include('conection.php');
+
+$result = mysqli_query($conn, $sql);
+mysqli_close($conn);
+
+header("location: ../pedidos.php");
+
 
 ?>
