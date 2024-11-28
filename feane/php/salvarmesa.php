b2aeaaf98deebdbbbf1fabe21909b9b71860220a
Gabriel Vinícius <gabriel_v_cruz@estudante.sesisenai.org.br>
2024-11-28 19:21:17 -0300

    essa poha dessa modal nn abre

--- feane/php/salvarmesa.php
+++ feane/php/salvarmesa.php
@@ -5,7 +5,8 @@ $ocp1 = $_POST["nOcp"];
 include("conection.php");
 
 $sql = "UPDATE mesa SET capacidade = $ocp1 WHERE id_mesa = $id;";
+
 $result = mysqli_query($conn,$sql);
 mysqli_close($conn);
-header("location: ../mesas.php");   
+// header("location: ../mesas.php");   
 ?>
