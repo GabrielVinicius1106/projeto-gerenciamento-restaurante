1f5b95b49a20cfe8f7c26702a64f2cf22d613c28
luluMachuca <furlanluisgustavo03@gmail.com>
2024-12-03 19:52:14 -0300

    8

--- feane/php/excluirMesa.php
+++ feane/php/excluirMesa.php
@@ -3,7 +3,7 @@ $id = $_GET["id"];
 include("conection.php");
 
 $sql = "DELETE FROM mesa WHERE id_mesa = $id;";
-$result = mysqli_query($conn,$sql);
+$rgit puesult = mysqli_query($conn,$sql);
 mysqli_close($conn);
 
 header("location: ../mesas.php");
