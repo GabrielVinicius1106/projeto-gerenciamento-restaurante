11f94d9d5a0beb8771227fb258a58328437b882f
DanDan <daniel_mn_costa@estudante.sc.senai.br>
2024-12-05 19:12:10 -0300

    não lembro o que eu mexi kkkkkk

--- feane/usuarios.php
+++ feane/usuarios.php
@@ -9,7 +9,6 @@
 <link rel="shortcut icon" href="dist/images/favicon.png" type="image/x-icon">
 <link rel="stylesheet" href="dist/css/elisson.css" />
 
-
 </head>
 <body>
     
@@ -18,22 +17,23 @@
       include("php/funcoes.php");
 ?>
 
-<a href="usuarios.php">Voltar</a>
+<a href="telainicialAdmin.php">Voltar</a>
 <h1 style="text-align: center;">Usuários</h1>
-<a href="adicionarUsuario.php"><input type="button" value="Adicionar Usuario"></a>
 
 <table id="tableCardapio"> 
       <tr>
-         <th>Nr Usuario</th>
-         <th>Dados</th>
-         <th>Cargo</th>
-         <th>Login</th>
-         <th>Senha</th>
-         <th>Editar</th>
+            <th>Nr Usuario</th>
+            <th>Dados</th>
+            <th>Cargo</th>
+            <th>Login</th>
+            <th>Senha</th>
+            <th>Editar</th>
       </tr>
       <?php 
          echo carregaUsuario();
-      ?>
+         ?>
 
+
+<a href="adicionarUsuario.php"><input type="button" class="ajustebotao" value="Adicionar Usuario"></a>
 </body>
 </html>
