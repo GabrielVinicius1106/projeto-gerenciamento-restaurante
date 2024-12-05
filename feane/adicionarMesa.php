2a3977e097ebafad079c9896fcc770f51415fa82
Gabriel Vinícius <gabrielviniciusdacruz546@gmail.com>
2024-12-04 21:11:06 -0300

    adicionando ícones

--- feane/adicionarMesa.php
+++ feane/adicionarMesa.php
@@ -10,6 +10,7 @@ include('php/global.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Mesa | Mesa</title>
+    <link rel="shortcut icon" href="dist/images/favicon.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="dist/css/elisson.css" />
     <link rel="stylesheet"  href="dist/css/cssBotao.css" />
     
--- feane/adicionarUsuario.php
+++ feane/adicionarUsuario.php
@@ -10,6 +10,7 @@ include('php/global.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Usuario | Usuarios</title>
+    <link rel="shortcut icon" href="dist/images/favicon.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="dist/css/elisson.css" />
     <link rel="stylesheet"  href="dist/css/cssBotao.css" />
 </head>
@@ -18,7 +19,7 @@ include('php/global.php');
 <form action="php/salvarUsario.php" method="POST">
     <a href="mesas.php">Voltar</a>
     <h1>Adicionar Usuario</h1>
-    <div class="container" id="additem">
+    <div class="container" id="additem">''
         <p>Dados pessoais: <input name="nDados" type="text" required></p>
         <p>Tipo de cargo: <input type="number" name="nCargo" required></p>
         <p>Login: <input name="nLogin" type="text" required></p>
