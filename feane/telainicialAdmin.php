5bf58aaea54c1ef556d579384308898386c1aeb2
Gabriel Vinícius <gabriel_v_cruz@estudante.sesisenai.org.br>
2024-12-05 21:44:42 -0300

    tela inical ruiim

--- feane/telainicialAdmin.php
+++ feane/telainicialAdmin.php
@@ -1,16 +1,3 @@
-<?php 
-session_start();
-$idTipoUsuario = $_SESSION['idTipoUsuario'];
-
-if($_GET['idTipoUsuario']){
-    $idTipoUsuario = $_GET['idTipoUsuario'];
-}   
-
-// var_dump($_SESSION['idTipoUsuario']);
-// die();
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -19,17 +6,18 @@ if($_GET['idTipoUsuario']){
     <title>Tela Inicial | Administração</title>
     <link rel="shortcut icon" href="dist/images/favicon.png" type="image/x-icon">
     <link href="dist/css/cssTelaInicial.css" rel="stylesheet" />
+    
 </head>
 <body id="bodytelainicial">
 
     <!-- Alerta criativo -->
-    <div id="alert-box" class="alert-box" style="display: none;">
+    <div id="alert-box" class="alert-box">
         <div class="alert-content">
             <img src="dist/images/about-img.png" class="alert-img">
             <div class="alert-text">
                 <h2>Bem-vindo!</h2>
                 <p>Estamos muito felizes de te ver</p>
-                <p>Cantina Pizzaria agradece a visita</p>
+                <p>Cantina Pizzaria agradeçe a visita</p>
                 <button id="close-alert">Fechar</button>
             </div>
         </div>
@@ -55,24 +43,18 @@ if($_GET['idTipoUsuario']){
                     <button id="btn4" class="btn btn-large">Caixa</button>
                 </div>
                 <div class="btn-item">
-                    <button id="btn5" class="btn btn-large"><a href="usuarios.php">Usuários</a></button>
+                    <button id="btn5" class="btn btn-large"><a href="usuarios.php" img src="dist/icones/usuario.png" class="alert-img">Usuários</button>
                 </div>
             </div>
         </div>
     </section>
 
     <script>
-        // Função para verificar se o alerta já foi exibido
+        // Exibe o alerta na tela
         window.onload = function() {
-            // Verifica no sessionStorage se o alerta já foi mostrado
-            if (!sessionStorage.getItem('alertShown')) {
-                setTimeout(function() {
-                    document.getElementById('alert-box').style.display = 'block';
-                }, 500);  // Exibe o alerta após meio segundo
-
-                // Marca o alerta como exibido na sessão
-                sessionStorage.setItem('alertShown', 'true');
-            }
+            setTimeout(function() {
+                document.getElementById('alert-box').style.display = 'block';
+            }, 500);  // Espera meio segundo antes de mostrar o alerta
         };
 
         // Fecha o alerta quando o botão é clicado
@@ -80,5 +62,8 @@ if($_GET['idTipoUsuario']){
             document.getElementById('alert-box').style.display = 'none';
         };
     </script>
+
+
+
 </body>
 </html>
