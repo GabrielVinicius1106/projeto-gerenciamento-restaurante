023a08c2fd7412e5844d3ee52410ffe546273a80
Gabriel Vinícius <gabrielviniciusdacruz546@gmail.com>
2024-12-05 21:09:04 -0300

    adicionando acesso á variável de tipoUsuario

--- feane/adicionarItem.php
+++ feane/adicionarItem.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+$idTipoUsuario = $_SESSION['idTipoUsuario'];
 
 include('php/global.php');
 
--- feane/telainicialAdmin.php
+++ feane/telainicialAdmin.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+$idTipoUsuario = $_SESSION['idTipoUsuario'];
 
 if($_GET['idTipoUsuario']){
     $idTipoUsuario = $_GET['idTipoUsuario'];
